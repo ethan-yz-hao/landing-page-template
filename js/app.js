4d04149ed79b33ec63469c2026d2cfb0abe34e52
Ethan Hao <hao.yz.ethan@gmail.com>
2024-01-15 15:39:19 -0500

    go to top

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
 /**
  *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
  * Dependencies: None
  *
  * JS Version: ES2015/ES6
  *
  * JS Standard: ESlint
  *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
  */
 
 /**
@@ -28,7 +18,12 @@ const sections = main.querySelectorAll('section');
 const n_section = sections.length;
 const nav_lst_frag = document.createDocumentFragment();
 const nav_lst = document.getElementById('navbar__list');
+const header = document.querySelector('header.main__hero');
 
+// Go to Top li
+const goto_top = document.createElement('li');
+goto_top.innerText = `Go To Top`;
+goto_top.className = 'menu__link';
 
 /**
  * End Global Variables
@@ -40,7 +35,7 @@ const nav_lst = document.getElementById('navbar__list');
 for (let i=0; i <n_section; i++) {
     const lst_item = document.createElement('li');
     lst_item.innerText = `Section ${i+1}`;
-    lst_item.className = 'menu__link'
+    lst_item.className = 'menu__link';
     nav_lst_frag.appendChild(lst_item);
 }
 nav_lst.appendChild(nav_lst_frag);
@@ -50,7 +45,6 @@ nav_lst.appendChild(nav_lst_frag);
 
 // Scroll to anchor ID using scrollTO event
 function scrollToSection(evt) {
-    console.log('A paragraph was clicked: ' + evt.target.textContent);
     const section_name = evt.target.innerText;
     document.querySelector(`section[data-nav="${section_name}"]`).scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
@@ -62,13 +56,33 @@ function makeActive(){
         const box = section.getBoundingClientRect();
         if (box.top <= 200 && box.bottom >= 300) {
             if (! section.classList.contains('your-active-class')) {
-                section.classList.add('your-active-class')
+                section.classList.add('your-active-class');
             }
         } else {
             if (section.classList.contains('your-active-class')) {
-                section.classList.remove('your-active-class')
+                section.classList.remove('your-active-class');
             }
         }
     }
 }
 document.addEventListener("scroll", makeActive)
+
+// Add go to top
+function checkTop() {
+    const box = header.getBoundingClientRect();
+    if (box.bottom <= 50) {
+        if (! nav_lst.contains(goto_top)) {
+            nav_lst.appendChild(goto_top);
+        }
+    } else {
+        if (nav_lst.contains(goto_top)) {
+            nav_lst.removeChild(goto_top);
+        }
+    }}
+document.addEventListener('scroll', checkTop);
+
+// Scroll to top
+function scrollToTop() {
+    window.scrollTo({top: 0, left: 0, behavior: "smooth"})
+}
+goto_top.addEventListener('click', scrollToTop);
